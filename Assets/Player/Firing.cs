32f9472a9d1d5abf39ff0a6c54d9fb99b606e6b1
Bansal Patel <48344337+htcexplorer2412@users.noreply.github.com>
2022-01-13 02:59:27 +1100

    Player controller (#1)
    * New Input System Controller
    
    * Add simple mouse look and movement
    
    * Fix Jumping and cleaning code
    
    * Add simple firing mechanics
    
    * Add Simple shooting mehcanics
    
    * Add enemy that follows player
    
    Co-authored-by: Bansal Patel <htcexplorer2410@gmail.com>

---
+++ Assets/Player/Firing.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Firing : MonoBehaviour
+{
+    public float damage = 10f;
+    [SerializeField] Camera mainCam; 
+
+   public void Shoot()
+    {
+        RaycastHit hit;
+        if(Physics.Raycast(mainCam.transform.position, mainCam.transform.forward,out hit))
+        {
+            Debug.Log(hit.transform.name);
+
+            Target target = hit.transform.GetComponent<Target>();
+            if(target != null)
+            {
+                target.TakeDamage(damage);
+            }
+        }
+    }
+}
---
+++ Assets/Player/InputManager.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InputManager : MonoBehaviour {
+#pragma warning disable 649
+
+    [SerializeField] Movement movement;
+    [SerializeField] MouseLook mouseLook;
+
+    [SerializeField] Firing firing;
+
+    PlayerControls controls;
+    PlayerControls.GroundMovementActions groundMovement;
+
+    Vector2 horizontalInput;
+    Vector2 mouseInput;
+
+    Coroutine fireCoroutine;
+
+    private void Awake ()
+    {
+        controls = new PlayerControls();
+        groundMovement = controls.GroundMovement;
+
+        // groundMovement.[action].performed += context => do something
+        groundMovement.HorizontalMovement.performed += ctx => horizontalInput = ctx.ReadValue<Vector2>();
+
+        groundMovement.Jump.performed += _ => movement.OnJumpPressed();
+
+        groundMovement.MouseX.performed += ctx => mouseInput.x = ctx.ReadValue<float>();
+        groundMovement.MouseY.performed += ctx => mouseInput.y = ctx.ReadValue<float>();
+
+        groundMovement.Shoot.started += _ => StartFiring();
+       // groundMovement.Shoot.canceled += _ => StopFiring();
+    }
+
+    private void Update ()
+    {
+        movement.ReceiveInput(horizontalInput);
+        mouseLook.ReceiveInput(mouseInput);
+    }
+
+    void StartFiring()
+    {
+        //fireCoroutine = StartCoroutine(gun.RapidFire());
+        firing.Shoot();
+    }
+
+    void StopFiring()
+    {
+        if (fireCoroutine != null) {
+            StopCoroutine(fireCoroutine);
+        }
+    }
+
+    private void OnEnable ()
+    {
+        controls.Enable();
+    }
+
+    private void OnDestroy ()
+    {
+        controls.Disable();
+    }
+}
